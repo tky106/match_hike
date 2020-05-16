5d634eed75f37e2fdf4faae1747f3c5330adbb22
tky106 <tktktky106@gmail.com>
2020-05-16 21:49:49 +0900

    日本語変換機能実装

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,11 @@ module MatchHike
   class Application < Rails::Application
     config.generators.template_engine = :slim 
     # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 6.0
-
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
