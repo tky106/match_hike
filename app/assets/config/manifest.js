e723e83f495a2e4fd63c589092aaa6b921ff0cb2
tky106 <tktktky106@gmail.com>
2020-05-26 01:29:18 +0900

    scss周り

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
 //= link_tree ../images
-//= link application.css
- //= link post.js
- //= link talk.js
-//= link_directory ../stylesheets .css
+//= link application.scss
+//= link post.js
+//= link talk.js
