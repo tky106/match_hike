b93e5ce956cbd1550d7821ac591b8add3374551c
tky106 <tktktky106@gmail.com>
2020-05-27 23:53:15 +0900

    jquery読み込みのための記述追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+//= require activestorage
+//= require turbolinks
 //= require jquery
 //= require jquery_ujs
+//= require_tree .
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
