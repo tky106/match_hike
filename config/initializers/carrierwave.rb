47a9f18eeadedff37753d2cb405cdcb836d1dd39
tky106 <tktktky106@gmail.com>
2020-05-31 16:48:08 +0900

    carrierwave.rb編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,22 @@
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+    config.storage = :file
+  elsif Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
     }
-    when 'production'
-      config.fog_directory  = 'match-hike'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/match-hike'
-      config.cache_storage = :fog
-    end  
+
+    config.fog_directory  = 'match-hike'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/match-hike'
+    config.cache_storage = :fog
   end
-end
+end  
