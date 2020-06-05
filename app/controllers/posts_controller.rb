68b7d347fb6823c2f67700ba5f7d2d9ca9d3697f
tky106 <tktktky106@gmail.com>
2020-06-05 21:47:35 +0900

    検索画面を未登録者でも使用可に変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :search]
   before_action :set_post, only: [:show, :edit, :update_apply, :complete, :update, :destroy]
 
   def index
